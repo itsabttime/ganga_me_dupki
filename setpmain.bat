1d2c195b127080b4bc3ef9a238eb87815a9ef7cf
itsabttime <117502862+itsabttime@users.noreply.github.com>
2023-03-29 21:12:50 +0530

    Add files via upload

--- setpmain.bat
+++ setpmain.bat
@@ -1,20 +1,59 @@
-@echo off
+
+setlocal enabledelayedexpansion
 set /a randum=%random%
 cls
 :read
-echo on
-set /p enterfilepath=
-echo off
-cls
+
+
+set /p enterrfilepath=
+
+set final=
+echo "!enterrfilepath!" | findstr /r "^!"&&GOTO EXCLAMATION
+echo gathring th harvest.pleasewait........
+PAUSE
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "*" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "^" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ ">" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "<" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "=" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "|" (set final=!final!!temp!) else (echo.ABOMINATIONS))
+set enterfilepath=!final!
+goto skp
+:purgenotneeded
+set enterfilepath=!enterrfilepath!
+:skp
 :::::: I am Jsust Christian ;//////
-echo.you've entered %enterfilepath%
-type %enterfilepath%&&CLS
-if %errorlevel% NEQ 0 ( cls&echo. There's something Wrong.&echo %enterfilepath% | findstr "^""" >nul&&goto read || goto dblqt ) else ( echo. ) 
+echo.you've entered "!enterfilepath!"
+echo !enterfilepath!
+PAUSE
+if %errorlevel% NEQ 0 ( echo.&echo. There's something Wrong.&&timeout 3 >nul&goto read)
+type !enterfilepath!&&PAUSE&CLS
+if %errorlevel% NEQ 0 ( echo. There's something Wrong.&echo %enterfilepath% | findstr "^""" >nul&&goto read || goto dblqt ) else ( echo. ) 
 goto yourdestiny
 :dblqt
 echo. I think u need to add "Double quotes" & timeout 2 >NUL
 goto read
 :yourdestiny
+for %%i in (!enterfilepath!) do echo %%i&set ffil=%%i
+echo !ffil!zzzzz
+start cmd /c "(for %%i in (!enterfilepath!) do set file=%%~nxi)&echo %file%&PAUSE"
+echo !file! !enterfilepath!zz "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!"
+PAUSE
+if NOT EXIST "%randum%.ganga.%file%" ( echo BAPTISED BY: Puneet Bapna >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!" ) else (goto :PAUSE)
+type !enterfilepath! >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!"
 
-for %%i in (%enterfilepath%) do set file=%%~nxi
-type %enterfilepath% ^>^> "%homedrive%\users\%username%\desktop\%randum%.ganga.%file%"
+:::::: Proud Christian ;//////
+:PAUSE
+echo. DNOE:)
+PAUSE
+EXIT
+:EXCLAMATION
+setlocal disabledelayedexpansion
+for %%i in (%enterrfilepath%) do CALL :file "%%i"
+:file
+set file="%~1"
+echo "%file%" | findstr /r "^!"
+setlocal enabledelayedexpansion
+if NOT EXIST "%randum%.ganga.%file%" ( echo BAPTISED BY: Puneet Bapna >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!" ) else (goto :PAUSE)
+type !file! >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!"
+PAUSE
