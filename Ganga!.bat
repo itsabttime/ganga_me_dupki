8c2c931f29d2c06619237879f63608df95e37dec
itsabttime <117502862+itsabttime@users.noreply.github.com>
2023-03-30 14:53:31 +0530

    Add files via upload

--- Ganga!.bat
+++ Ganga!.bat
@@ -1,4 +1,3 @@
-
 setlocal enabledelayedexpansion
 set /a randum=%random%
 cls
@@ -8,15 +7,36 @@ cls
 set /p enterrfilepath=
 
 set final=
-echo "!enterrfilepath!" | findstr /r "^!"&&GOTO EXCLAMATION
+echo "!enterrfilepath!"&echo "!enterrfilepath!" | findstr /r "^!"&PAUSE
+echo "!enterrfilepath!" | findstr /r "^!"&&GOTO EXCLAMATION || CALL :NORMAL "!enterrfilepath!"
 echo gathring th harvest.pleasewait........
+echo off
+
+:EXCLAMATION
+setlocal disabledelayedexpansion
+for %%i in (%enterrfilepath%) do CALL :file "%%i"
+:file
+set file="%~1"
+echo "%file%" | findstr /r "^!"
+setlocal enabledelayedexpansion
+if NOT EXIST "%randum%.ganga.%file%" ( echo BAPTISED BY: Puneet Bapna >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!" ) else (goto :PAUSE)
+type !file! >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!"
 PAUSE
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "*" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "^" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ ">" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "<" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "=" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
-(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&if "!temp!" NEQ "|" (set final=!final!!temp!) else (echo.ABOMINATIONS))
+endlocal
+endlocal
+endlocal
+:NORMAL
+echo. Welcome Normal
+PAUSE
+echo "%~1"&PAUSE
+set enterrfilepath="!enterrfilepath!"
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "*" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "^" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ ">" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "<" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "=" (set final=!final!!temp!) else (echo.ABOMINATIONS))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "!" (set final=!final!!temp!) else (set temp=!final!&setlocal disabledelayedexpansion &set final=%temp%!&endlocal enabledelayedexpansion))&set enterrfilepath=!final!&set final=
+(for /l %%i in (0,1,250) do set temp=!enterrfilepath:~%%i,1!&echo !temp!&if "!temp!" NEQ "" if "!temp!" NEQ "|" (set final=!final!!temp!) else (echo.ABOMINATIONS))
 set enterfilepath=!final!
 goto skp
 :purgenotneeded
@@ -47,13 +67,3 @@ type !enterfilepath! >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!fi
 echo. DNOE:)
 PAUSE
 EXIT
-:EXCLAMATION
-setlocal disabledelayedexpansion
-for %%i in (%enterrfilepath%) do CALL :file "%%i"
-:file
-set file="%~1"
-echo "%file%" | findstr /r "^!"
-setlocal enabledelayedexpansion
-if NOT EXIST "%randum%.ganga.%file%" ( echo BAPTISED BY: Puneet Bapna >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!" ) else (goto :PAUSE)
-type !file! >> "%homedrive%\users\%username%\desktop\%randum%.ganga.!file!"
-PAUSE
